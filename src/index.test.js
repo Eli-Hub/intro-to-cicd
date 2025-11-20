1b453d0ba14393a95bfb64dd5f55d25670917777
Eli-Hub <emmajay099@gmail.com>
2025-11-20 09:16:18 +0000

    added trivial comment

--- src/index.test.js
+++ src/index.test.js
@@ -2,6 +2,7 @@
 
 const sayHi = require('./index.js')
 
+// This test checks if sayHi function returns the correct greeting
 test("Says 'Hello Mike' when 'Mike' is passed", () => {
    expect(sayHi('Mike')).toBe('Hello Mike')
 })
